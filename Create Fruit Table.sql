24dbdcc93de78cc6cb001820e25f61e2004fb455
JayneRamos <jramos125@student.mtsac.edu>
2016-04-28 15:49:07 -0700

    Create Fruit Table

---
+++ Create Fruit Table.sql
@@ -0,0 +1,10 @@
+USE strawberrymitten;
+
+CREATE TABLE fruits 
+(plu char(4) not null primary key, 
+ fruitName char(25) not null, 
+ fruitDescription char(25) not null, 
+ fruitNotes char(40) not null, 
+ fruitPrice float(4,2)not null
+ );
+
