f3b835c59b4cc136b7ebc1cfd493d8c0ff0bff6d
JayneRamos <jramos125@student.mtsac.edu>
2016-05-11 14:05:13 -0700

    Update Create DB + Tables.sql

--- Create DB + Tables.sql
+++ Create DB + Tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE Customers
   cust_zip      char(10)  NOT NULL ,
   cust_email    char(255) NOT NULL ,
   cust_password char(64)  NOT NULL ,
-  admin         tinyint   NOT NULL
+  administration tinyint   NOT NULL
 );
 
 -- -----------------------
