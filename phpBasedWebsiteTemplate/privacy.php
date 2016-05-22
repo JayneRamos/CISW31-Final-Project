564edef238d95146375504b869cc6af291dbd51e
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-22 12:02:18 -0700

    Add files via upload

--- phpBasedWebsiteTemplate/privacy.php
+++ phpBasedWebsiteTemplate/privacy.php
@@ -3,8 +3,9 @@
 ?>
 
 <div class="privacy">
-	<p> Strawberry Mittens will never sell, share, rent or lease any personal information gathered from our site.</p>
-	<p> Your privacy is improtant to us. We strive to make your shopping experiance a positive one. We value you as a customer.</p>
+	<h2>We value you as a customer.</h2>
+	<p> Strawberry Mitte will never sell, share, rent or lease any personal information gathered from our site.</p>
+	<p> Your privacy is improtant to us. We strive to make your shopping experiance a positive one.</p>
 
 </div>
 
