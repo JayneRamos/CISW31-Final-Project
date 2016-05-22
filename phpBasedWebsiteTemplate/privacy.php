c83cd18f548059a78ee88c71911341878d3137ae
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-22 12:04:07 -0700

    Add files via upload

--- phpBasedWebsiteTemplate/privacy.php
+++ phpBasedWebsiteTemplate/privacy.php
@@ -2,8 +2,9 @@
 	require('header.php');
 ?>
 
+<h2>We value you as a customer.</h2>
+
 <div class="privacy">
-	<h2>We value you as a customer.</h2>
 	<p> Strawberry Mitte will never sell, share, rent or lease any personal information gathered from our site.</p>
 	<p> Your privacy is improtant to us. We strive to make your shopping experiance a positive one.</p>
 
