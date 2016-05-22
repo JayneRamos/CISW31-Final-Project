72824d6908ffc9a39c1b1a063dc8dcd73b1c78f6
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-22 10:44:20 -0700

    Create index.php

---
+++ phpBasedWebsiteTemplate/index.php
@@ -0,0 +1,17 @@
+<?php
+  require('header.php');
+?>
+  <!-- page content -->
+  <div class="content">
+  	<p>
+  		Welcome to the home of Strawberry Mitten.
+  		Please take some time to get to know us.
+  	</p>
+ 	 <p>
+ 	 	We specialize in serving your business needs
+  		and hope to hear from you soon!
+  	</p>
+  </div>
+<?php
+  require('footer.php');
+?>
