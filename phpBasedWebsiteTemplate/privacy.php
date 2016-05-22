abac39f1696ee2acbc37c3a6a7e03913de8c5fa6
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-22 10:44:58 -0700

    Create privacy.php

---
+++ phpBasedWebsiteTemplate/privacy.php
@@ -0,0 +1,13 @@
+<?php
+	require('header.php');
+?>
+
+<div class="privacy">
+	<p> Strawberry Mittens will never sell, share, rent or lease any personal information gathered from our site.</p>
+	<p> Your privacy is improtant to us. We strive to make your shopping experiance a positive one. We value you as a customer.</p>
+
+</div>
+
+<?php
+	require('footer.php');
+?>
