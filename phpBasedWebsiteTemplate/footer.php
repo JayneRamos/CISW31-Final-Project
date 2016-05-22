edd82a198affa7435a7a2097161739acd1112830
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-22 10:42:44 -0700

    Create footer.php

---
+++ phpBasedWebsiteTemplate/footer.php
@@ -0,0 +1,13 @@
+<!-- page footer -->
+
+<table class="foot" width=î100%î cellpadding=î12î border=î0î>
+	<tr>
+		<td>
+			<p class="foot">&copy; 2016 Strawberry Mitten</p>
+			<p class="foot">Please review our <a href=privacy.php>Privacy Policy</a></p>
+		</td>
+	</tr>
+</table>
+
+</body>
+</html>
