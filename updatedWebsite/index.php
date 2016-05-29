dc93ea20c868b58290f51dc45a4df4a6107f87b9
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-05-29 15:57:23 -0700

    Add files via upload

---
+++ updatedWebsite/index.php
@@ -0,0 +1,18 @@
+<?php
+  require('header.php');
+?>
+  <!-- page content -->
+  <div class="content">
+  	
+  	<h2>Welcome to the home of Strawberry Mitten.</h2>
+  	
+  	<p>	Please take some time to get to know us.
+  	</p>
+ 	 <p>
+ 	 	We specialize in serving your business needs
+  		and hope to hear from you soon!
+  	</p>
+  </div>
+<?php
+  require('footer.php');
+?>
