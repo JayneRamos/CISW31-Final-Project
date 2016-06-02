df649a9cef9466301d8b3a1dd5530502943a8b53
JayneRamos <jramos125@student.mtsac.edu>
2016-06-02 16:35:49 -0700

    Update Privacy Policy

--- updatedWebsite/privacy.php
+++ updatedWebsite/privacy.php
@@ -2,11 +2,37 @@
 	require('header.php');
 ?>
 
-<h2>We value you as a customer.</h2>
+<h1>PRIVACY POLICY FOR STRAWBERRY MITTEN EXOTIC FRUIT</h1>
 
 <div class="content">
-	<p> Strawberry Mitte will never sell, share, rent or lease any personal information gathered from our site.</p>
-	<p> Your privacy is improtant to us. We strive to make your shopping experiance a positive one.</p>
+	<h2>Our Commitment to Privacy</h2>
+		<p> Your privacy is important to us. To better protect your privacy we provide this notice explaining our online 
+		information practices and the choices you can make about the way your information is collected and used. 
+		To make this notice easy to find, we make it available on our homepage and at every point where personally 
+		identifiable information may be requested.</p>
+	<h2>The Information We Collect: </h2>
+		<p> This notice applies to all information collected or submitted to us directly or on the Strawberry Mitten 
+		Exotic Fruit website. On some pages, you can order products, make requests, and register to receive materials. 
+		The types of personal information collected at these pages are:</p>
+		<p>Name</br>
+		   Address</br>
+		   Email Address</br>
+		   Phone Number</br>
+		   Credit/Debit Card Information</p>
+	<h2>The Way We Use Information: </h2>
+		<p> We use the information you provide about yourself when placing an order only to complete that order. 
+		We do not share this information with outside parties except to the extent necessary to complete that order. 
+		We use the information you provide about someone else when placing an order only to ship the product and to 
+		confirm delivery. We do not share this information with outside parties except to the extent necessary to 
+		complete that order. We use return email addresses to answer the email we receive. Such addresses are not 
+		shared with outside parties.</p>
+	<h2>Our Commitment To Data Security</h2>
+		<p> To prevent unauthorized access, maintain data accuracy, and ensure the correct use of information, 
+		we have put in place appropriate physical, electronic, and managerial procedures to safeguard and secure 
+		the information we collect online.</p>
+	<h2>How To Contact Us</h2>
+		<p> Should you have other questions or concerns about these privacy policies, please call us at 909-592-1002 
+		or send us an email at Sookie@SrawberryMitten.</p>
 
 </div>
 
