e2f783ff394ab7ab2d8823a5359162c8e666a047
Anthony Coe <AnthonyCoe@users.noreply.github.com>
2016-06-05 16:57:05 -0700

    Add files via upload

--- updatedWebsite/index.php
+++ updatedWebsite/index.php
@@ -3,8 +3,10 @@
 ?>
   <!-- page content -->
   <div class="homepage">
-  	
   	<img src="images/Balinese-Fruit_Stand-P2.jpg" alt="fruit stand" align = "middle" width = "100%" height = "100%"/>
+  </div>
+  <div class = "slogan">
+  	<img src="images/slogan.png" alt="fruit irrifruitable" align = "middle" width = "75%" height = "auto"/>  		
   </div>
 <?php
   require('footer.php');
